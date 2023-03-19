5f0c191f9eff956782b42e205ddb4ad7671bd617
Tan Sakeer <tansakeer@gmail.com>
2023-03-19 04:19:59 +0000

    Dashboard completed Removed bugs and finished exchange rate.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,26 +4,40 @@ def index
   render( { :template => "/project_template/index.html.erb"})
 end
 
-def forex_first
+def forex
   @raw_data= open("https://api.exchangerate.host/symbols").read
   @parsed_data = JSON.parse(@raw_data)
   @symbols_hash= @parsed_data.fetch("symbols")
 
   @array_of_symbols= @symbols_hash.keys
-  render( { :template => "/project_template/forex_first.html.erb"})
+  render( { :template => "/project_template/forex.html.erb"})
 end
 
-def forex_second
+def forex_first
+  @starting_currency = params.fetch("one")
+
   @raw_data= open("https://api.exchangerate.host/symbols").read
   @parsed_data = JSON.parse(@raw_data)
   @symbols_hash= @parsed_data.fetch("symbols")
   
   @array_of_symbols= @symbols_hash.keys
-  render( { :template => "/project_template/forex_second.html.erb"})
+
+  render( { :template => "/project_template/forex_first.html.erb"})
 end
 
 def forex_result
+
+  @starting_currency = params.fetch("one")
+  @ending_currency = params.fetch("two")
+
+    @raw_data = open("https://api.exchangerate.host/convert?from=#{@starting_currency}&to=#{@ending_currency}").read
+    @parsed_data = JSON.parse(@raw_data)
+    @info_hash = @parsed_data.fetch("info")
+    @info_rate = @info_hash["rate"] 
+
+    render({ :template => "project_template/forex_result.html.erb" })
 end
+
 
 def covid_calc
   render( { :template => "/project_template/covid.html.erb"})
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get("/", { :controller => "index", :action => "index" })
-  get("/forex", { :controller => "index", :action => "forex_first" })
-  get("/forex/:one", { :controller => "index", :action => "forex_calc" })
+  get("/forex", { :controller => "index", :action => "forex" })
+  get("/forex/:one", { :controller => "index", :action => "forex_first" })
+  get("/forex/:one/:two", { :controller => "index", :action => "forex_result" })
 
 
-  get("/covid", { :controller => "index", :action => "covid_calc" })
+
+  #get("/covid", { :controller => "index", :action => "covid_calc" })
 
 end
